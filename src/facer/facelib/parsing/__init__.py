c98191352c5cb0ca8b757f99295821cb2b8376b8
zain5266 <zaindatalytics@gmail.com>
2023-09-13 15:41:09 +0500

    update

--- src/facer/facelib/parsing/__init__.py
+++ src/facer/facelib/parsing/__init__.py
@@ -2,7 +2,7 @@ import os
 import requests
 import torch
 
-from facelib.utils import load_file_from_url
+from facer.facelib.utils import load_file_from_url
 from .bisenet import BiSeNet
 from .parsenet import ParseNet
 
