6f5d51322a3b98cfaf59fb0bf9e3e9267af51fa5
zain5266 <zaindatalytics@gmail.com>
2023-09-13 15:33:08 +0500

    update

--- src/facer/facelib/utils/face_restoration_helper.py
+++ src/facer/facelib/utils/face_restoration_helper.py
@@ -4,11 +4,11 @@ import os
 import torch
 from torchvision.transforms.functional import normalize
 
-from facelib.detection import init_detection_model
-from facelib.parsing import init_parsing_model
-from facelib.utils.misc import img2tensor, imwrite, is_gray, bgr2gray, adain_npy
-from basicsr.utils.download_util import load_file_from_url
-from basicsr.utils.misc import get_device
+from facer.facelib.detection import init_detection_model
+from facer.facelib.parsing import init_parsing_model
+from facer.facelib.utils.misc import img2tensor, imwrite, is_gray, bgr2gray, adain_npy
+from facer.basicsr.utils.download_util import load_file_from_url
+from facer.basicsr.utils.misc import get_device
 
 dlib_model_url = {
     'face_detector': 'https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/mmod_human_face_detector-4cb19393.dat',
